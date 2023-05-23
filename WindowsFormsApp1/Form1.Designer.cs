3a84dc2eaf57da000d51ff0caa905e70e17870d2
Kavslen <kavaslen@yandex.ru>
2023-05-24 00:21:10 +0300

    first commit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -34,16 +34,23 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(753, 63);
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(753, 95);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(475, 368);
+            this.listBox1.Size = new System.Drawing.Size(475, 364);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
@@ -53,15 +60,17 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(900, 510);
+            this.button2.Location = new System.Drawing.Point(879, 510);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(259, 126);
+            this.button2.Size = new System.Drawing.Size(276, 112);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
@@ -87,11 +96,55 @@
             this.numericUpDown1.Size = new System.Drawing.Size(244, 29);
             this.numericUpDown1.TabIndex = 4;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(52, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(52, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Группа";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(52, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Возраст";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.Location = new System.Drawing.Point(749, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Список";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1301, 837);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -115,6 +168,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -25,7 +25,7 @@ namespace WindowsFormsApp1
             {
                 conn.Open();
             }
-            String insSql = "INSERT INTO students(name, age, groupName) VALUES (@name, @age, @groupName)";
+            String insSql = "INSERT INTO spisok(name, age, groupName) VALUES (@name, @age, @groupName)";
             MySqlCommand insConn = new MySqlCommand(insSql, conn);
             insConn.Parameters.AddWithValue("@name", textBox1.Text.Trim());
             insConn.Parameters.AddWithValue("@age", numericUpDown1.Value);
@@ -38,14 +38,9 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            String connStr = "Server=localhost;User ID=exem_pk31;Password=123456;Database=exam_pk31";
+            String connStr = "Server=localhost;User ID=kavaslen;Password=123456;Database=exam_pk31";
             conn = new MySqlConnection(connStr);
             conn.Open();
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -54,14 +49,20 @@ namespace WindowsFormsApp1
             {
                 conn.Open();
             }
-            String sqlSel = "SELECT name, age, groupName FROM students";
+            String sqlSel = "SELECT name, age, groupName FROM spisok";
             MySqlCommand sqlComm = new MySqlCommand(sqlSel, conn);
             MySqlDataReader sqlDataReader = sqlComm.ExecuteReader();
             while (sqlDataReader.Read())
             {
-                listBox1.Items.Add(sqlDataReader.GetString(0) + "" + sqlDataReader.GetInt32(1) + "" + sqlDataReader.GetString(2));
+                listBox1.Items.Add(sqlDataReader.GetString(0) + "" + sqlDataReader.GetString(1) + "" + sqlDataReader.GetString(2));
+
             }
             conn.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
